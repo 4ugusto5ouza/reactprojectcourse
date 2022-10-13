0d7e7e6bd05b8246a6851256f38fd00deb2110c6
Samuel Souza <augusto_souza@edu.unifil.br>
2022-10-12 21:02:42 -0300

    :sparkles:feat: atualizado proj para react 18

--- movie-list/src/index.js
+++ movie-list/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { List } from "./containers/List";
 
 const App = () => {
   return <List />;
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
